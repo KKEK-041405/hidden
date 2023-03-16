15e139467ca8626b7651fbfe50a802c9359b9958
KKEK <komaleshwarakumarkonatham@gmail.com>
2023-03-16 22:00:20 +0530

    login complet

--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView,LogoutView
+from django.views.generic import TemplateView
 
+class LoginPage(LoginRequiredMixin,LoginView):
+    success_url = "/details"
+    def get_context_data(self, **kwargs):
+        context = super(LoginPage, self).get_context_data(**kwargs)
+        context["succes_url"] = self.request.GET.get(next) or self.success_url
+        return context
+    
+class DetailsPage(TemplateView):
+    template_name = "details.html"
+
+class IndexPage(TemplateView):
+    template_name = "home.html"
+
+
+class LogoutPage(LogoutView):
+    pass
 # Create your views here.
--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from app.urls import urlpatterns as app_urls
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("",include(app_urls)),
 ]
